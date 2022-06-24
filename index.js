745bdc07afd5d03c40745b7a008402746ba12f46
Fernanda Freitas Martinez <idea.fernanda@gmail.com>
2022-06-24 14:29:32 +0100

    javascript crud

---
+++ index.js
@@ -0,0 +1,58 @@
+const socialMedia = {
+  users: [
+    {
+      username: "fernandafreitas"
+    }
+  ],
+  posts: [
+    {
+      id: 1,
+      owner: "fernandafreias",
+      content: "First post"
+    }
+  ]
+}
+
+// CREATE
+function createPost(data) {
+  socialMedia.posts.push({
+    id: socialMedia.posts.length + 1,
+    owner: data.owner,
+    content: data.content
+   })
+}
+
+// READ
+function getPosts() {
+  return socialMedia.posts;
+}
+
+// UPDATE
+function updatePostContent(id, content) {
+  const findPost = getPosts().find((post) => {
+    return post.id !== id;
+  })
+  return findPost.content = content;
+}
+
+// DELETE
+function deletePost(id) {
+  const updatedPostsList = getPosts().filter((post) => {
+    return post.id !== id;
+  });
+  return socialMedia.posts = updatedPostsList;
+}
+
+// Creating tree Posts
+createPost({owner: "fernandafreitas", content: "Second post"});
+createPost({owner: "fernandafreitas", content: "Third post"});
+createPost({owner: "fernandafreitas", content: "Fourth post"});
+
+// Updating content from Second Post
+updatePostContent(2, "Updated post content");
+
+// Deleting First Post
+deletePost(1);
+
+// Reading all Posts
+console.log(getPosts());
